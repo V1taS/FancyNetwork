9e9b66b556d23e819e60c174a3bc2108c4a31bf1
V1taS <SosinVitalii@iCloud.com>
2023-08-11 17:51:34 +0200

    Initial commit

---
+++ Sources/FancyNetwork/FancyNetwork.swift
@@ -0,0 +1,6 @@
+public struct FancyNetwork {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/FancyNetworkTests/FancyNetworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FancyNetwork
+
+final class FancyNetworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FancyNetwork().text, "Hello, World!")
+    }
+}
